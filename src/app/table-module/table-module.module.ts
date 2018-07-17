14680c26429970d9d14804ca95c2b69b993a8f77
vikas magar <vikasmagar512@gmail.com>
2018-07-17 12:38:17 +0530

    Pagination working

--- src/app/table-module/table-module.module.ts
+++ src/app/table-module/table-module.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 import {TableDemoComponent} from './table-demo/table-demo.component';
 import { Ng2TableModule } from 'ng2-table/ng2-table';
 import { ReusableTableComponent } from './reusable-table/reusable-table.component';
+import {FormsModule} from '@angular/forms';
+import {PaginationModule} from 'ngx-bootstrap';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    PaginationModule.forRoot(),
     Ng2TableModule
   ],
   declarations: [TableDemoComponent, ReusableTableComponent],
