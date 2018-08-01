c229989826378f1ebbb0a7a7dc2f25e88610a1b2
vikas magar <vikasmagar512@gmail.com>
2018-08-01 12:05:54 +0530

    sign in date modifications

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -16,6 +16,7 @@ export class SignInComponent implements OnInit {
   public isBusy = false;
   public hasFailed = false;
   public showInputErrors = false;
+  public now: Date = new Date();
 
   constructor(
     private api: ApiService,
@@ -59,9 +60,9 @@ export class SignInComponent implements OnInit {
     const password = this.frm.get('password').value;
 
     // Submit request to API
-    
+
     this.router.navigate(['main']);
-    /* this.api
+     /*this.api
       .signIn(username, password)
       .subscribe(
         (response:any) => {
@@ -77,6 +78,6 @@ export class SignInComponent implements OnInit {
           this.isBusy = false;
           this.hasFailed = true;
         }
-      ); */
+      );*/
   }
 }
